05231cdf27529bb3990fcc305e2e07ae97c17c5a
Vladyslav Pavlik <pavliklaw7@gmail.com>
2022-07-15 21:41:22 +0300

    userName in header fix

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="header__inner">
         <NuxtLink class="header__link" to="/">HOME</NuxtLink>
-        <p v-if="user">
+        <p v-if="user" class="header__link">
           {{ `${user.first_name} ${user.last_name}` }}
         </p>
         <NuxtLink v-if="!isAuthenticated" class="header__link" to="/login"
